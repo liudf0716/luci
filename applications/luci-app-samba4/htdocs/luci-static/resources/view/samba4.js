ba5121091e2ceb2689f200d0c30ab0363c66003d
staylightblow8 <liudf0716@gmail.com>
2023-06-17 17:04:34 +0800

    luci-app-samba4: simplify samba's config ui
    Signed-off-by: staylightblow8 <liudf0716@gmail.com>

--- applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4.js
+++ applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4.js
@@ -112,48 +112,6 @@ return view.extend({
 		o.default = 'no'; // smb.conf default is 'yes'
 		o.rmempty = false;
 
-		s.option(form.Flag, 'force_root', _('Force Root'));
-
-		o = s.option(form.Value, 'users', _('Allowed users'));
-		o.rmempty = true;
-
-		o = s.option(form.Flag, 'guest_ok', _('Allow guests'));
-		o.enabled = 'yes';
-		o.disabled = 'no';
-		o.default = 'yes'; // smb.conf default is 'no'
-		o.rmempty = false;
-
-		o = s.option(form.Flag, 'guest_only', _('Guests only'));
-		o.enabled = 'yes';
-		o.disabled = 'no';
-		o.default = 'no';
-
-		o = s.option(form.Flag, 'inherit_owner', _('Inherit owner'));
-		o.enabled = 'yes';
-		o.disabled = 'no';
-		o.default = 'no';
-
-		o = s.option(form.Value, 'create_mask', _('Create mask'));
-		o.maxlength = 4;
-		o.default = '0666'; // smb.conf default is '0744'
-		o.placeholder = '0666';
-		o.rmempty = false;
-
-		o = s.option(form.Value, 'dir_mask', _('Directory mask'));
-		o.maxlength = 4;
-		o.default = '0777'; // smb.conf default is '0755'
-		o.placeholder = '0777';
-		o.rmempty = false;
-
-		o = s.option(form.Value, 'vfs_objects', _('Vfs objects'));
-		o.rmempty = true;
-
-		s.option(form.Flag, 'timemachine', _('Apple Time-machine share'));
-
-		o = s.option(form.Value, 'timemachine_maxsize', _('Time-machine size in GB'));
-		o.rmempty = true;
-		o.maxlength = 5;
-
 		return m.render();
 	}
 });
