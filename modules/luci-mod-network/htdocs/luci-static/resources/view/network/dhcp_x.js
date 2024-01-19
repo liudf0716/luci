79e23a73f04b2030e2f1aa4447784a7275846d03
Dengfeng Liu <liudf0716@gmail.com>
2024-01-19 16:21:22 +0800

    luci-mod-network: modify dhcp page for chinese
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/dhcp_x.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/dhcp_x.js
@@ -11,23 +11,25 @@ return view.extend({
     render: function () {
         var m, s, o;
 
-        m = new form.Map('dhcp', _('DHCP'));
-        m.description = _('DHCP configuration');
+        m = new form.Map('dhcp', _('DHCP Server'));
+        m.description = _('DHCP Server General Settings');
 
-        s = m.section(form.NamedSection, 'lan', 'dhcp', _('DHCP configuration'));
+        s = m.section(form.NamedSection, 'lan', 'dhcp', _('General Setup'));
         
-        o = s.option(form.Flag, 'ignore', _('Dont start DHCP server'));
+        o = s.option(form.Flag, 'ignore', _('Ignore interface'), 
+            _("Disable <abbr title=\"Dynamic Host Configuration Protocol\">DHCP</abbr> for this interface."));
         o.rmempty = false;
 
-        o = s.option(form.Value, 'start', _('Start address'));
+        o = s.option(form.Value, 'start', _('Start'), _('Lowest leased address as offset from the network address.'));
         o.datatype = 'integer';
         o.rmempty = false;
 
-        o = s.option(form.Value, 'limit', _('Limit'));
+        o = s.option(form.Value, 'limit', _('Limit'), _('Maximum number of leased addresses.'));
         o.datatype = 'integer';
         o.rmempty = false;
 
-        o = s.option(form.Value, 'leasetime', _('Lease time'));
+        o = s.option(form.Value, 'leasetime', _('Lease time'), 
+            _('Expiry time of leased addresses, minimum is 2 minutes (<code>2m</code>).'));
         o.datatype = 'string';
         o.rmempty = false;
 
