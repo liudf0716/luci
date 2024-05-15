910d2fd2072af43b544a52e67423cbed6eb3f6d7
Dengfeng Liu <liudf0716@gmail.com>
2024-05-15 16:23:41 +0800

    luci-base: add github stats feature
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>
    (cherry picked from commit eaf543008e2f5daac512efc0ef8d7547e81e1441)
    (cherry picked from commit d1a4192f63a92ce8acd04aaca09680d273968eac)

--- modules/luci-base/htdocs/luci-static/resources/tools/github.js
+++ modules/luci-base/htdocs/luci-static/resources/tools/github.js
@@ -2,15 +2,50 @@
 
 return L.Class.extend({
     desc: function(description, username, project) {
-        return "<table style='border: 0;'>" +
+        var luci_project = 'luci-app-' + project;
+        var title = _('if you have any problem, please click to view the project on GitHub : ') + project;
+        var luci_title = _('if you have any problem, please click to view the luci ui project on GitHub : ') + luci_project;
+        var package_label = 'package-' + project.replace(/-/g, '_');
+        var luci_label = 'luci_ui-' + luci_project.replace(/-/g, '_');
+
+        return "<table style='border: 0; table-layout: auto;'>" +
                     "<tr>" +
                         "<td style='border: 0;'>" + _(description) + "</td>" +
                         "<td style='border: 0;'>" +
-                            "<a href='https://github.com/" + username + "/" + project + "' target='_blank'>" +
+                            "<table style='border: 0; table-layout: auto;'>" +
+                                "<tr>" +
+                                    "<td style='border: 0;'>" +
+                                        "<a href='https://github.com/" + username + "/" + project + "' target='_blank' title='" + title + "'>" +
+                                            "<img alt='" + project + "' src='https://img.shields.io/badge/'" + package_label + "/>" +
+                                            "<img alt='" + project + "' src='https://img.shields.io/github/stars/" + username + "/" + project + "?style=social' />" +
+                                        "</a>" +
+                                        "<a href='https://github.com/" + username + "/" + luci_project + "' target='_blank' title='" + luci_title + "'>" +
+                                            "<img alt='" + luci_project + "' src='https://img.shields.io/badge/'" + luci_label + "/>" +
+                                            "<img alt='" + luci_project + "' src='https://img.shields.io/github/stars/" + username + "/" + luci_project + "?style=social' />" +
+                                        "</a>" +
+                                    "</td>" +
+                                "</tr>" +
+                            "</table>" +
+                        "</td>" +
+                    "</tr>" +
+                "</table>";
+    },
+
+    luci_desc: function(description, username, project) {
+        project = 'luci-app-' + project;
+        var luci_title = _('if you have any problem, please click to view the luci ui project on GitHub : ') + project;
+        var luci_label = 'luci_ui-' + project.replace(/-/g, '_');
+
+        return "<table style='border: 0; table-layout: auto;'>" +
+                    "<tr>" +
+                        "<td style='border: 0;'>" + _(description) + "</td>" +
+                        "<td style='border: 0;'>" +
+                            "<a href='https://github.com/" + username + "/" + project + "' target='_blank' title='" + luci_title + "'>" +
+                                "<img alt='" + project + "' src='https://img.shields.io/badge/'" + luci_label + "/>" +
                                 "<img alt='" + project + "' src='https://img.shields.io/github/stars/" + username + "/" + project + "?style=social' />" +
                             "</a>" +
                         "</td>" +
                     "</tr>" +
                 "</table>";
     }
-}); 
+});
