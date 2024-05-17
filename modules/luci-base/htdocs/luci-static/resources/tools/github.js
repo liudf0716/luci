629e8c68fa97ca214326fccd1289c38cbf84d242
Dengfeng Liu <liudf0716@gmail.com>
2024-05-17 09:34:13 +0800

    luci-base: fix github tool bug
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>
    (cherry picked from commit 13b89f479ac7adcb34a1471feac6ed88ab53ee2b)
    (cherry picked from commit 1de641e3821eb867941e78befd39f709c7524165)

--- modules/luci-base/htdocs/luci-static/resources/tools/github.js
+++ modules/luci-base/htdocs/luci-static/resources/tools/github.js
@@ -5,8 +5,8 @@ return L.Class.extend({
         var luci_project = 'luci-app-' + project;
         var title = _('if you have any problem, please click to view the project on GitHub : ') + project;
         var luci_title = _('if you have any problem, please click to view the luci ui project on GitHub : ') + luci_project;
-        var package_label = 'package-' + project.replace(/-/g, '_');
-        var luci_label = 'luci_ui-' + luci_project.replace(/-/g, '_');
+        var package_label = 'package-' + project.replace(/-/g, '_') + '-default';
+        var luci_label = 'luci-' + project.replace(/-/g, '_') + '-default';
 
         return "<table style='border: 0; table-layout: auto;'>" +
                     "<tr>" +
@@ -16,11 +16,11 @@ return L.Class.extend({
                                 "<tr>" +
                                     "<td style='border: 0;'>" +
                                         "<a href='https://github.com/" + username + "/" + project + "' target='_blank' title='" + title + "'>" +
-                                            "<img alt='" + project + "' src='https://img.shields.io/badge/'" + package_label + "/>" +
+                                            "<img alt='" + project + "' src='https://img.shields.io/badge/" + package_label +  "' />" +
                                             "<img alt='" + project + "' src='https://img.shields.io/github/stars/" + username + "/" + project + "?style=social' />" +
                                         "</a>" +
                                         "<a href='https://github.com/" + username + "/" + luci_project + "' target='_blank' title='" + luci_title + "'>" +
-                                            "<img alt='" + luci_project + "' src='https://img.shields.io/badge/'" + luci_label + "/>" +
+                                            "<img alt='" + luci_project + "' src='https://img.shields.io/badge/" + luci_label + "' />" +
                                             "<img alt='" + luci_project + "' src='https://img.shields.io/github/stars/" + username + "/" + luci_project + "?style=social' />" +
                                         "</a>" +
                                     "</td>" +
@@ -32,16 +32,16 @@ return L.Class.extend({
     },
 
     luci_desc: function(description, username, project) {
+        var luci_label = 'luci-' + project.replace(/-/g, '_') + '-default';
         project = 'luci-app-' + project;
         var luci_title = _('if you have any problem, please click to view the luci ui project on GitHub : ') + project;
-        var luci_label = 'luci_ui-' + project.replace(/-/g, '_');
-
+        
         return "<table style='border: 0; table-layout: auto;'>" +
                     "<tr>" +
                         "<td style='border: 0;'>" + _(description) + "</td>" +
                         "<td style='border: 0;'>" +
                             "<a href='https://github.com/" + username + "/" + project + "' target='_blank' title='" + luci_title + "'>" +
-                                "<img alt='" + project + "' src='https://img.shields.io/badge/'" + luci_label + "/>" +
+                                "<img alt='" + project + "' src='https://img.shields.io/badge/" + luci_label + "' />" +
                                 "<img alt='" + project + "' src='https://img.shields.io/github/stars/" + username + "/" + project + "?style=social' />" +
                             "</a>" +
                         "</td>" +
