16fd20216436815754c113d02fb07d46c060055c
zhangzengfei <zh.zengfei@gmail.com>
2025-04-18 03:16:25 +0800

    luci-app-snmpd: Simplify setup and include trap configuration for AgentX
    Signed-off-by: zhangzengfei <zh.zengfei@gmail.com>

--- applications/luci-app-snmpd/htdocs/luci-static/resources/view/snmpd/snmpd.js
+++ applications/luci-app-snmpd/htdocs/luci-static/resources/view/snmpd/snmpd.js
@@ -3,6 +3,7 @@
 'use strict';
 'require form';
 'require ui';
+'require uci';
 'require view';
 
 var desc = _(""
@@ -14,51 +15,73 @@ var desc = _(""
 );
 
 return view.extend({
-	render: function() {
-		let m, s, o;
+	render: async function() {
+		let m, s, o, t;
 
-		m = new form.Map("snmpd", _("net-snmp's SNMPD"), desc);
+		m = new form.Map("snmpd", _("SNMPD"));
 
 		s = m.section(form.TypedSection, "agent", _("Agent settings"));
 		s.anonymous = true;
 		o = s.option(form.Value, "agentaddress", _("The address the agent should listen on"),
 			_("Eg: UDP:161, or UDP:10.5.4.3:161 to only listen on a given interface"));
 
-		s = m.section(form.TypedSection, "agentx", _("AgentX settings"),
-			_("Delete this section to disable AgentX"));
+		s = m.section(form.TypedSection, "agentx", _("AgentX settings"));
 		s.anonymous = true;
 		o = s.option(form.Value, "agentxsocket", _("The address the agent should allow AgentX connections to"),
 			_("This is only necessary if you have subagents using the agentX "
 			+ "socket protocol. Eg: /var/run/agentx.sock"));
-		s.addremove = true;
+		// s.addremove = true;
 
-		s = m.section(form.TypedSection, "com2sec", _("com2sec security"));
-		o = s.option(form.Value, "secname", "secname");
-		o = s.option(form.Value, "source", "source");
-		o = s.option(form.Value, "community", "community");
+		// s = m.section(form.TypedSection, "com2sec", _("com2sec security"));
+		// o = s.option(form.Value, "secname", "secname");
+		// o = s.option(form.Value, "source", "source");
+		// o = s.option(form.Value, "community", "community");
 
-		s = m.section(form.TypedSection, "group", "group", _("Groups help define access methods"));
-		s.addremove = true;
-		s.option(form.Value, "group", "group");
-		s.option(form.Value, "version", "version");
-		s.option(form.Value, "secname", "secname");
+		// s = m.section(form.TypedSection, "group", "group", _("Groups help define access methods"));
+		// s.addremove = true;
+		// s.option(form.Value, "group", "group");
+		// s.option(form.Value, "version", "version");
+		// s.option(form.Value, "secname", "secname");
 
-		s = m.section(form.TypedSection, "access", "access");
-		s.option(form.Value, "group", "group");
-		s.option(form.Value, "context", "context");
-		s.option(form.Value, "version", "version");
-		s.option(form.Value, "level", "level");
-		s.option(form.Value, "prefix", "prefix");
-		s.option(form.Value, "read", "read");
-		s.option(form.Value, "write", "write");
-		s.option(form.Value, "notify", "notify");
+		// s = m.section(form.TypedSection, "access", "access");
+		// s.option(form.Value, "group", "group");
+		// s.option(form.Value, "context", "context");
+		// s.option(form.Value, "version", "version");
+		// s.option(form.Value, "level", "level");
+		// s.option(form.Value, "prefix", "prefix");
+		// s.option(form.Value, "read", "read");
+		// s.option(form.Value, "write", "write");
+		// s.option(form.Value, "notify", "notify");
 
 		s = m.section(form.TypedSection, "system", _("System"), _("Values used in the MIB2 System tree"));
 		s.anonymous = true;
-		s.option(form.Value, "sysLocation", "sysLocation");
-		s.option(form.Value, "sysContact", "sysContact");
-		s.option(form.Value, "sysName", "sysName");
+		s.option(form.Value, "sysLocation", _("sys Location"));
+		s.option(form.Value, "sysContact", _("Contact"));
+		s.option(form.Value, "sysName", _("sysName"));
 
-		return m.render();
+		// agentx 配置
+		t = new form.Map("agentx");
+		s = t.section(form.TypedSection, "trap", _("Trap settings"));
+		s.anonymous = true;
+
+		o = s.option(form.Value, "host", _("server Address"));
+		o.datatype = "ip4addr";
+
+		o = s.option(form.Value, "port", _("server Port"));
+		o.datatype = "port";
+		o.placeholder = 162;
+
+		o = s.option(form.Value, "interval", _("interval"), _("Time interval for continuous alarms"));
+		o.datatype = "uinteger";
+		o.placeholder = 600;
+
+		return Promise.all([m.render(), t.render()]).then(function(rendered) {
+			const container = document.createElement('div');
+			rendered.forEach(function(el) {
+				container.appendChild(el);
+			});
+
+			return container;
+		});
 	}
 });
